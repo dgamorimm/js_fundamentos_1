bce85c45e05d1fe7a7420900a061ee4c5d5830e8
douglasamorim-house <douglas@gavea.com>
2021-08-19 15:32:20 -0300

    adicionando o arquivo describe-types.js

---
+++ describe-types.js
@@ -0,0 +1,16 @@
+// tipagem dinâmica
+// untyped
+// não precisamos dizer qual tipo é minha variavel, ele automaticamente infere
+
+let minhaVar = 567;
+minhaVar = "texto";
+minhaVar = true;
+
+// Multiparadigma - consegue resolver o mesmo problema de varias formas diferentes
+// ES2015 e ES6 são as versões mais famosas do JS
+
+
+// Liguagem interpretada - JS e Pythonv - O programa exesuta direto a sua linha de código
+// Liguagem compilada - Java - O progama compila a liguagem e gera um outro código em liguagem de máquina
+
+// NodeJS - foi criado pararodar o código javascript no nosso computador e não no navegador.
