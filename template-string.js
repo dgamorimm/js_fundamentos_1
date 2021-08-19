145281a282e3b2a1fcb5c7a67731e895aa20104d
douglasamorim-house <douglas@gavea.com>
2021-08-19 16:12:22 -0300

    adicionando o arquivo template-string.js

---
+++ template-string.js
@@ -0,0 +1,7 @@
+const nome = "Douglas";
+const idade = 2021-1994;
+const cidadeDeNascimento = "Rio de Janeiro";
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade do ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
